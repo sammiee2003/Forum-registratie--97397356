ba14f4a76dd7fb4994a492e91cc49dcae0f8f81c
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-04-06 09:41:39 +0200

    lvl 4 opdr 3

--- registratie_handler.php
+++ registratie_handler.php
@@ -1,17 +1,12 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-	foreach($_POST as $key => $val)
-	{
-		if ($key == 'fname')
-		{
-			echo 'username:'.$val."<br>";
-        } elseif($key == 'lname'){
-            echo "uw email is:" . $val. "<br>";
-        }
-		else
-		{
-			echo "uw leeftijd is:" . $val."<br>";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	foreach ($_POST as $key => $val) {
+		if ($key == 'fname') {
+			echo 'username:'. $val . "<br>";
+        } elseif($key == 'lname') {
+            echo "uw email is:" . $val . "<br>";
+        } else {
+			echo "uw leeftijd is:" . $val . "<br>";
 		}
 	}
 }
